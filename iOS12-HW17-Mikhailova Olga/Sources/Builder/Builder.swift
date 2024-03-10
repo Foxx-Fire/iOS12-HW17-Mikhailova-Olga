22a2153615718296eeb4df8d96144f37e79f3862
Olga Mikhailova <foxx_fire@mail.ru>
2024-03-10 18:37:56 +0300

    Structure

---
+++ iOS12-HW17-Mikhailova Olga/Sources/Builder/Builder.swift
@@ -0,0 +1,8 @@
+//
+//  Builder.swift
+//  iOS12-HW17-Mikhailova Olga
+//
+//  Created by FoxxFire on 10.03.2024.
+//
+
+import Foundation
---
+++ iOS12-HW17-Mikhailova Olga/Sources/PasswordPresenter/PasswordPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  PasswordPresenter.swift
+//  iOS12-HW17-Mikhailova Olga
+//
+//  Created by FoxxFire on 10.03.2024.
+//
+
+import Foundation
